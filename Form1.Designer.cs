52050f0332ab8c4032074d7511c0eb117dd37bac
feldway1 <feldway120@gmail.com>
2024-06-17 11:13:53 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,288 @@
+﻿namespace CSharp_dkmonhoc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtmssv = new System.Windows.Forms.TextBox();
+            this.txthovaten = new System.Windows.Forms.TextBox();
+            this.cbxnienkhoa = new System.Windows.Forms.ComboBox();
+            this.cbxlop = new System.Windows.Forms.ComboBox();
+            this.rdb1 = new System.Windows.Forms.RadioButton();
+            this.rdb2 = new System.Windows.Forms.RadioButton();
+            this.rdb3 = new System.Windows.Forms.RadioButton();
+            this.rdb4 = new System.Windows.Forms.RadioButton();
+            this.clbmonhoc = new System.Windows.Forms.CheckedListBox();
+            this.btndky = new System.Windows.Forms.Button();
+            this.btnhuy = new System.Windows.Forms.Button();
+            this.btnthoat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(237, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG KÝ MÔN HỌC";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(109, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "MSSV";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Họ và tên ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(73, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Niên khóa";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(127, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Lớp";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(101, 283);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 23);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Học kỳ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(85, 332);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(82, 23);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Môn học";
+            // 
+            // txtmssv
+            // 
+            this.txtmssv.Location = new System.Drawing.Point(226, 80);
+            this.txtmssv.Name = "txtmssv";
+            this.txtmssv.Size = new System.Drawing.Size(282, 30);
+            this.txtmssv.TabIndex = 7;
+            this.txtmssv.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txthovaten
+            // 
+            this.txthovaten.Location = new System.Drawing.Point(226, 129);
+            this.txthovaten.Name = "txthovaten";
+            this.txthovaten.Size = new System.Drawing.Size(282, 30);
+            this.txthovaten.TabIndex = 8;
+            // 
+            // cbxnienkhoa
+            // 
+            this.cbxnienkhoa.FormattingEnabled = true;
+            this.cbxnienkhoa.Location = new System.Drawing.Point(226, 185);
+            this.cbxnienkhoa.Name = "cbxnienkhoa";
+            this.cbxnienkhoa.Size = new System.Drawing.Size(282, 31);
+            this.cbxnienkhoa.TabIndex = 11;
+            this.cbxnienkhoa.SelectedIndexChanged += new System.EventHandler(this.cbxnienkhoa_SelectedIndexChanged);
+            // 
+            // cbxlop
+            // 
+            this.cbxlop.FormattingEnabled = true;
+            this.cbxlop.Location = new System.Drawing.Point(226, 235);
+            this.cbxlop.Name = "cbxlop";
+            this.cbxlop.Size = new System.Drawing.Size(282, 31);
+            this.cbxlop.TabIndex = 12;
+            // 
+            // rdb1
+            // 
+            this.rdb1.AutoSize = true;
+            this.rdb1.Location = new System.Drawing.Point(226, 281);
+            this.rdb1.Name = "rdb1";
+            this.rdb1.Size = new System.Drawing.Size(35, 27);
+            this.rdb1.TabIndex = 13;
+            this.rdb1.TabStop = true;
+            this.rdb1.Text = "I";
+            this.rdb1.UseVisualStyleBackColor = true;
+            // 
+            // rdb2
+            // 
+            this.rdb2.AutoSize = true;
+            this.rdb2.Location = new System.Drawing.Point(280, 283);
+            this.rdb2.Name = "rdb2";
+            this.rdb2.Size = new System.Drawing.Size(42, 27);
+            this.rdb2.TabIndex = 14;
+            this.rdb2.TabStop = true;
+            this.rdb2.Text = "II";
+            this.rdb2.UseVisualStyleBackColor = true;
+            // 
+            // rdb3
+            // 
+            this.rdb3.AutoSize = true;
+            this.rdb3.Location = new System.Drawing.Point(341, 283);
+            this.rdb3.Name = "rdb3";
+            this.rdb3.Size = new System.Drawing.Size(49, 27);
+            this.rdb3.TabIndex = 15;
+            this.rdb3.TabStop = true;
+            this.rdb3.Text = "III";
+            this.rdb3.UseVisualStyleBackColor = true;
+            // 
+            // rdb4
+            // 
+            this.rdb4.AutoSize = true;
+            this.rdb4.Location = new System.Drawing.Point(409, 283);
+            this.rdb4.Name = "rdb4";
+            this.rdb4.Size = new System.Drawing.Size(46, 27);
+            this.rdb4.TabIndex = 16;
+            this.rdb4.TabStop = true;
+            this.rdb4.Text = "IV";
+            this.rdb4.UseVisualStyleBackColor = true;
+            // 
+            // clbmonhoc
+            // 
+            this.clbmonhoc.FormattingEnabled = true;
+            this.clbmonhoc.Location = new System.Drawing.Point(226, 332);
+            this.clbmonhoc.Name = "clbmonhoc";
+            this.clbmonhoc.Size = new System.Drawing.Size(282, 179);
+            this.clbmonhoc.TabIndex = 17;
+            // 
+            // btndky
+            // 
+            this.btndky.Location = new System.Drawing.Point(64, 557);
+            this.btndky.Name = "btndky";
+            this.btndky.Size = new System.Drawing.Size(122, 37);
+            this.btndky.TabIndex = 18;
+            this.btndky.Text = "ĐĂNG KÝ";
+            this.btndky.UseVisualStyleBackColor = true;
+            this.btndky.Click += new System.EventHandler(this.btndky_Click);
+            // 
+            // btnhuy
+            // 
+            this.btnhuy.Location = new System.Drawing.Point(280, 557);
+            this.btnhuy.Name = "btnhuy";
+            this.btnhuy.Size = new System.Drawing.Size(123, 37);
+            this.btnhuy.TabIndex = 19;
+            this.btnhuy.Text = "HỦY";
+            this.btnhuy.UseVisualStyleBackColor = true;
+            // 
+            // btnthoat
+            // 
+            this.btnthoat.Location = new System.Drawing.Point(495, 557);
+            this.btnthoat.Name = "btnthoat";
+            this.btnthoat.Size = new System.Drawing.Size(148, 37);
+            this.btnthoat.TabIndex = 20;
+            this.btnthoat.Text = "THOÁT";
+            this.btnthoat.UseVisualStyleBackColor = true;
+            this.btnthoat.Click += new System.EventHandler(this.btnthoat_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(699, 624);
+            this.Controls.Add(this.btnthoat);
+            this.Controls.Add(this.btnhuy);
+            this.Controls.Add(this.btndky);
+            this.Controls.Add(this.clbmonhoc);
+            this.Controls.Add(this.rdb4);
+            this.Controls.Add(this.rdb3);
+            this.Controls.Add(this.rdb2);
+            this.Controls.Add(this.rdb1);
+            this.Controls.Add(this.cbxlop);
+            this.Controls.Add(this.cbxnienkhoa);
+            this.Controls.Add(this.txthovaten);
+            this.Controls.Add(this.txtmssv);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "Form1";
+            this.Text = "ĐĂNG KÝ MÔN HỌC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtmssv;
+        private System.Windows.Forms.TextBox txthovaten;
+        private System.Windows.Forms.ComboBox cbxnienkhoa;
+        private System.Windows.Forms.ComboBox cbxlop;
+        private System.Windows.Forms.RadioButton rdb1;
+        private System.Windows.Forms.RadioButton rdb2;
+        private System.Windows.Forms.RadioButton rdb3;
+        private System.Windows.Forms.RadioButton rdb4;
+        private System.Windows.Forms.CheckedListBox clbmonhoc;
+        private System.Windows.Forms.Button btndky;
+        private System.Windows.Forms.Button btnhuy;
+        private System.Windows.Forms.Button btnthoat;
+    }
+}
+
